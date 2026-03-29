73757bf1a606b123892b24d8a253a86a8cb5b9c6
shilpayadav3196-arch <shilpayadav3196@gmail.com>
2026-03-29 16:25:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ import os
 
 # ------------------ PAGE CONFIG ------------------
 st.set_page_config(page_title="Career Guidance System", page_icon="🎯", layout="wide")
-# ------------------ LOGO ------------------
-st.sidebar.image("logo.png", width=120)
-
-col1, col2, col3 = st.columns([1,2,1])
-with col2:
-    st.image("logo.png", width=150)
 
 # ------------------ STYLING ------------------
 st.markdown("""
